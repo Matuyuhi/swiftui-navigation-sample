e7520502899afd3997de846134796bec95cccd28
Matuyuhi <bird9.yuhi@gmail.com>
2023-10-11 22:21:42 +0900

    Initial Commit

---
+++ navigation-sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  navigation-sample
+//
+//  Created by yuhi y on 2023/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ navigation-sample/navigation_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigation_sampleApp.swift
+//  navigation-sample
+//
+//  Created by yuhi y on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct navigation_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
